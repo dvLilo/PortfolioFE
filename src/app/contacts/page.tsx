e96e7710dcce3bd285e3eabf41d72414bb140d1b
Limay Louie Ducut <louie.ocampo27@gmail.com>
2024-04-01 16:33:17 +0800

    Added intro container to contact page.

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,11 +1,24 @@
 "use client"
 
+
 import { motion } from "framer-motion"
 
+import "@app/contacts/contact.styles.scss"
+
 const ContactPage = () => {
+
   return (
-    <motion.main className="limayyProjectPage" initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 0.3, duration: 0.3, ease: "easeIn" } }} >
-      <h1>This is contacts page!</h1>
+    <motion.main className="limayyContactPage" initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 0.3, duration: 0.3, ease: "easeIn" } }}>
+      <div className="limayyContactPage__intro">
+        <div className="limayyContactPage__wrapper">
+          <div>
+            <h1>contacts.</h1>
+          </div>
+          <div>
+            <h1>contacts.</h1>
+          </div>
+        </div>
+      </div>
     </motion.main>
   )
 }
